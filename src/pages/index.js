cbc9ea9b0d3b669bf77afe0dfefd943c682e2856
onox <denkpadje@gmail.com>
2022-09-01 02:17:12 +0200

    Move code blocks to hero banner of landing page
    Fixes: #1

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,72 @@ import React from "react"
 import Link from "@docusaurus/Link"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import useIsBrowser from "@docusaurus/useIsBrowser"
+import CodeBlock from "@theme/CodeBlock"
 import Layout from "@theme/Layout"
+import TabItem from "@theme/TabItem"
+import Tabs from "@theme/Tabs"
 
 import HomepageFeatures from "@site/src/components/HomepageFeatures"
 
 import clsx from "clsx"
 
 import styles from "./index.module.css"
+
+const sampleA = `with Ada.Text_IO;
+
+procedure Main is
+   type GUID is new String (1 .. 32)
+     with Dynamic_Predicate => (for all C of GUID => C in '0' .. '9' | 'a' .. 'f');
+
+   ID_1 : constant GUID := "030000004c050000cc09000011810000";
+begin
+   Ada.Text_IO.Put_Line ("Reading from device " & String (ID_1) & "...");
+end Main;
+`
+
+// https://github.com/onox/xoshiro/blob/master/src/xoshiro128.ads
+//
+// Copyright (c) 2022 onox
+// SPDX-License-Identifier: Apache-2.0
+const sampleB = `with Interfaces;
+
+package Xoshiro128 with Pure, SPARK_Mode => On is
+   use type Interfaces.Unsigned_64;
+
+   type Generator is limited private;
+
+   procedure Next (S : in out Generator; Value : out Interfaces.Unsigned_32)
+     with Global  => null,
+          Depends => (S => S, Value => S);
+
+   procedure Reset (S : out Generator; Seed : Interfaces.Unsigned_64)
+     with Global  => null,
+          Depends => (S => Seed),
+          Pre     => Seed /= 0;
+
+private
+   type Generator is array (0 .. 3) of Interfaces.Unsigned_32;
+end Xoshiro128;`
+
+// https://github.com/JeremyGrosser/pico_examples/blob/master/ravenscar_blink/src/main.adb
+//
+// Copyright (c) 2021 Jeremy Grosser
+// SPDX-License-Identifier: BSD-3-Clause
+const sampleC = `with RP.GPIO;
+with Pico;
+
+procedure Main is
+begin
+   RP.GPIO.Enable;
+
+   Pico.LED.Configure (RP.GPIO.Output);
+   Pico.LED.Set;
+
+   loop
+      Pico.LED.Toggle;
+      delay 0.1;
+   end loop;
+end Main;`
 
 const targets = [
   ["Win", "windows"],
@@ -77,19 +136,34 @@ function HomepageHeader() {
 
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <p>Get started with Alire, the Ada package manager</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to={platformDownloadURL}>
-            Download Alire {alireVersion.slice(0)} {platformLabel}
-          </Link>
-        </div>
+      <div className={clsx("container", styles.columns)}>
         <div className="container">
-          <small>
-            Download for {join(allPlatformLinks, <span>, </span>)}, or {linkOthers}
-          </small>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <p>Get started with Alire, the Ada package manager</p>
+          <div className={styles.buttons}>
+            <Link className="button button--secondary button--lg" to={platformDownloadURL}>
+              Download Alire {alireVersion.slice(0)} {platformLabel}
+            </Link>
+          </div>
+          <div className="container">
+            <small>
+              Download for {join(allPlatformLinks, <span>, </span>)}, or {linkOthers}
+            </small>
+          </div>
+        </div>
+        <div className={clsx("container", styles.heroTabs)}>
+          <Tabs>
+            <TabItem value="basic" label="Basic">
+              <CodeBlock showLineNumbers>{sampleA}</CodeBlock>
+            </TabItem>
+            <TabItem value="spark" label="SPARK">
+              <CodeBlock showLineNumbers>{sampleB}</CodeBlock>
+            </TabItem>
+            <TabItem value="embedded" label="Embedded">
+              <CodeBlock showLineNumbers>{sampleC}</CodeBlock>
+            </TabItem>
+          </Tabs>
         </div>
       </div>
     </header>
