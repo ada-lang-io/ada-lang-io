86a6b05254ada1be5a7f9919a062f976b43d8fea
onox <denkpadje@gmail.com>
2022-09-07 11:08:37 +0200

    Wrap Admonition component for AARM admonitions
    Use <Admonition type="aarm" aarm="KIND"> where KIND is one of the keys
    in `aarmTypes` in src/theme/Admonition/index.js.

---
+++ src/theme/Admonition/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+import Admonition from "@theme-original/Admonition"
+
+import styles from "./styles.module.css"
+
+// Possible types are: note, tip, info, caution, danger
+const aarmTypes = {
+  reason: "info",
+  discussion: "info",
+  "implementation-advice": "tip",
+  "implementation-defined": "caution",
+  "implementation-note": "caution"
+}
+
+export default function AdmonitionWrapper(props) {
+  if (props.type === "aarm") {
+    return (
+      <div className={styles.aarm}>
+        <Admonition {...props} type={aarmTypes[props.aarm] ?? "note"} />
+      </div>
+    )
+  }
+  return <Admonition {...props} />
+}
