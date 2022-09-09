40dacc1eaa19cc7b45d9ddae207a7d0ada491c04
onox <denkpadje@gmail.com>
2022-09-09 14:28:17 +0200

    Reimplement hero of landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,31 @@ import React from "react"
 import Link from "@docusaurus/Link"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import useIsBrowser from "@docusaurus/useIsBrowser"
-import CodeBlock from "@theme/CodeBlock"
 import Layout from "@theme/Layout"
-import TabItem from "@theme/TabItem"
-import Tabs from "@theme/Tabs"
 
 import HomepageFeatures from "@site/src/components/HomepageFeatures"
+import DownloadIcon from "@site/static/img/fontawesome/solid/download.svg"
+
+import {
+  Button,
+  Container,
+  Group,
+  MantineProvider,
+  Overlay,
+  Tabs,
+  Text,
+  Title
+} from "@mantine/core"
+import { useOs } from "@mantine/hooks"
+import { Prism } from "@mantine/prism"
+
+import clsx from "clsx"
+
+import styles from "./index.module.scss"
 
 import sampleA from "!!raw-loader!./code-basic.adb"
 import sampleC from "!!raw-loader!./code-rp2040.adb"
 import sampleB from "!!raw-loader!./code-spark.ads"
-import clsx from "clsx"
-
-import styles from "./index.module.scss"
 
 // sampleB from src/xoshiro128.ads in https://github.com/onox/xoshiro
 //
@@ -26,22 +38,6 @@ import styles from "./index.module.scss"
 //
 // Copyright (c) 2021 Jeremy Grosser
 // SPDX-License-Identifier: BSD-3-Clause
-
-const targets = [
-  ["Win", "windows"],
-  ["Linux", "linux"],
-  ["Mac", "macos"]
-]
-
-function getPlatformKey(platform) {
-  for (const [prefix, key] of targets) {
-    if (platform.startsWith(prefix)) {
-      return key
-    }
-  }
-
-  return null
-}
 
 const installTargets = new Map([
   ["windows", { label: "Windows", urlSuffix: "installer-x86_64-windows.exe" }],
@@ -65,79 +61,122 @@ function getInstallTarget(version, suffix) {
   return `${gitHubProjectPage}/releases/download/v${version}/alr-${version}-${suffix}`
 }
 
-function HomepageHeader() {
+function CodeBlock({ showLineNumbers, children }) {
+  // <Prism> supports scrollAreaComponent="div" for native scroll area
+  return (
+    <Prism withLineNumbers={showLineNumbers} language="ada" showLineNumbers>
+      {children}
+    </Prism>
+  )
+}
+
+export function HomepageHeader({ title, description }) {
   const isBrowser = useIsBrowser()
-  const { siteConfig } = useDocusaurusContext()
+  const os = useOs()
 
-  const platformKey = isBrowser
-    ? getPlatformKey(navigator.userAgentData?.platform || navigator.platform)
-    : null
+  const platformKey = isBrowser ? os : null
+
   const platform = platformKey !== null ? installTargets.get(platformKey) : null
-
   const platformLabel = platform !== null ? ` for ${platform.label}` : ""
 
   const platformDownloadURL =
     platform !== null ? getInstallTarget(alireVersion, platform.urlSuffix) : gitHubReleasePage
 
   const allPlatformLinks = Array.from(installTargets.values()).map(({ label, urlSuffix }) => (
-    <Link className={styles.heroLink} to={getInstallTarget(alireVersion, urlSuffix)}>
-      {label}
-    </Link>
+    <Link to={getInstallTarget(alireVersion, urlSuffix)}>{label}</Link>
   ))
 
-  const linkOthers = (
-    <Link className={styles.heroLink} to={gitHubReleasePage}>
-      others
-    </Link>
-  )
+  const linkOthers = <Link to={gitHubReleasePage}>others</Link>
 
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className={clsx("container", styles.columns)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <p>Get started with Alire, the Ada package manager</p>
-          <div className={styles.buttons}>
-            <Link className="button button--secondary button--lg" to={platformDownloadURL}>
-              Download Alire {alireVersion.slice(0)} {platformLabel}
-            </Link>
-          </div>
-          <div className="container">
-            <small>
+    <header className={styles.heroWrapper}>
+      <Overlay color="#000" opacity={0.65} zIndex={1} />
+
+      <div className={styles.heroInner}>
+        <div className={styles.columns}>
+          <div>
+            <Title className={styles.title}>{title}</Title>
+
+            <Container size={640}>
+              <Text size="lg" className={styles.description}>
+                {description}
+              </Text>
+              <Text size="lg" className={styles.description}>
+                Get started with Alire, the Ada package manager.
+              </Text>
+            </Container>
+
+            <Group position="center" className={styles.controls}>
+              <Button
+                className={styles.controlPrimary}
+                variant="white"
+                size="md"
+                component={Link}
+                to={platformDownloadURL}
+                variant="gradient"
+                //gradient={{ from: 'green', to: 'teal' }}
+                gradient={{ from: "blue", to: "cyan" }}
+                leftIcon={<DownloadIcon className={styles.downloadIcon} />}
+              >
+                Download Alire {alireVersion.slice(0)} {platformLabel}
+              </Button>
+              <Button
+                className={styles.controlSecondary}
+                size="md"
+                component={Link}
+                to="/docs/getting-started/why-ada"
+              >
+                Get Started
+              </Button>
+            </Group>
+
+            <Text size="xs" className={styles.textDownloadLinks}>
               Download for {join(allPlatformLinks, <span>, </span>)}, or {linkOthers}
-            </small>
+            </Text>
           </div>
-        </div>
-        <div className={clsx("container", styles.heroTabs)}>
-          <Tabs>
-            <TabItem value="basic" label="Basic">
-              <CodeBlock showLineNumbers>{sampleA}</CodeBlock>
-            </TabItem>
-            <TabItem value="spark" label="SPARK">
-              <CodeBlock showLineNumbers>{sampleB}</CodeBlock>
-            </TabItem>
-            <TabItem value="embedded" label="Embedded">
-              <CodeBlock showLineNumbers>{sampleC}</CodeBlock>
-            </TabItem>
-          </Tabs>
+          <div>
+            <div className={styles.heroTabs}>
+              <Tabs color="blue" variant="pills" defaultValue="embedded">
+                <Tabs.List>
+                  <Tabs.Tab value="ada">Ada</Tabs.Tab>
+                  <Tabs.Tab value="spark">SPARK</Tabs.Tab>
+                  <Tabs.Tab value="embedded">Embedded</Tabs.Tab>
+                </Tabs.List>
+
+                <div className={styles.codeTabPanel}>
+                  <Tabs.Panel value="ada" pt="xs">
+                    <CodeBlock showLineNumbers>{sampleA}</CodeBlock>
+                  </Tabs.Panel>
+                  <Tabs.Panel value="spark" pt="xs">
+                    <CodeBlock showLineNumbers>{sampleB}</CodeBlock>
+                  </Tabs.Panel>
+                  <Tabs.Panel value="embedded" pt="xs">
+                    <CodeBlock showLineNumbers>{sampleC}</CodeBlock>
+                  </Tabs.Panel>
+                </div>
+              </Tabs>
+            </div>
+          </div>
         </div>
       </div>
     </header>
   )
 }
 
-function Home() {
+export default function Home() {
   const { siteConfig } = useDocusaurusContext()
 
   return (
-    <Layout title={siteConfig.title} description={siteConfig.customFields.description}>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <MantineProvider theme={{ colorScheme: "dark" }}>
+      <Layout title={siteConfig.title} description={siteConfig.customFields.description}>
+        <HomepageHeader
+          title={siteConfig.title}
+          description={siteConfig.customFields.description}
+        />
+        <main>
+          <HomepageFeatures />
+        </main>
+      </Layout>
+    </MantineProvider>
   )
 }
-
-export default Home
