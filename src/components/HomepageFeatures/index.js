5f39ca8e33f110f619aa3475555243efa0e0de6b
onox <denkpadje@gmail.com>
2022-09-09 18:31:57 +0200

    Use Mantine for features on landing page
    Images and code blocks are still missing. Subtitle and description may
    need to be modified.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,63 +4,61 @@ import iconPerformance from "@site/static/img/chip-svgrepo-com.svg"
 import iconCorrectness from "@site/static/img/plane-svgrepo-com.svg"
 import iconReadability from "@site/static/img/student-reading-svgrepo-com.svg"
 
-import clsx from "clsx"
+import { Container, SimpleGrid, Text, Title } from "@mantine/core"
 
-import styles from "./styles.module.scss"
+import classes from "./index.module.scss"
 
-function Feature({ icon, title, children }) {
-  const Svg = icon
-
+function Feature({ title, subTitle, description, items }) {
   return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className={clsx("padding-horiz--md", styles.featureText)}>
-        <h3>{title}</h3>
-        {children}
-      </div>
-    </div>
+    <section className={classes.sectionWrapper}>
+      <Container size={700}>
+        <Text className={classes.title}>{title}</Text>
+        <Title className={classes.subTitle} order={2}>
+          {subTitle}
+        </Title>
+
+        <Container size={660} p={0}>
+          <Text color="dimmed" className={classes.description}>
+            {description}
+          </Text>
+        </Container>
+
+        {!!items && (
+          <SimpleGrid
+            cols={2}
+            spacing={50}
+            breakpoints={[{ maxWidth: 550, cols: 1, spacing: 40 }]}
+            style={{ marginTop: 30 }}
+          >
+            {items}
+          </SimpleGrid>
+        )}
+      </Container>
+    </section>
   )
 }
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          <Feature icon={iconReadability} title="Readability">
-            <p>
-              Express intent with explicitness and keywords over symbols and special structures.
-            </p>
-            <p>
-              Express concepts like meaning in integers. Use built-in design by contract with
-              pre/post-conditions and invariants. Model problems with typechecks and range
-              constraints.
-            </p>
-          </Feature>
-          <Feature icon={iconCorrectness} title="Correctness">
-            <p>
-              Build with technology used in 40 years of reliability in planes, trains, and
-              spaceships.
-            </p>
-            <p>
-              Use the SPARK subset to formally verify part or all of your program, and integrate
-              existing SPARK crates available in the Alire package manager.
-            </p>
-          </Feature>
-          <Feature icon={iconPerformance} title="Performance">
-            <p>
-              Build native applications and take advantage of other libraries through binding to C
-              and C++.
-            </p>
-            <p>
-              Use inline assembly or compiler intrinsics when you need it. Control resources with
-              scope-based resource control (RAII) and your own memory allocators.
-            </p>
-          </Feature>
-        </div>
-      </div>
-    </section>
+    <div className={classes.features}>
+      <Feature
+        title="Readability"
+        subTitle="Express intent with explicitness and keywords over symbols and special structures"
+        description="Express concepts like meaning in integers. Use built-in design by contract with pre/post-conditions and invariants. Model problems with typechecks and range constraints."
+        item={undefined}
+      />
+      <Feature
+        title="Correctness"
+        subTitle="Build with technology used in 40 years of reliability in planes, trains, and spaceships"
+        description="Use the SPARK subset to formally verify part or all of your program, and integrate existing SPARK crates available in the Alire package manager."
+        item={undefined}
+      />
+      <Feature
+        title="Performance"
+        subTitle="Build native applications and take advantage of other libraries through binding to C and C++"
+        description="Use inline assembly or compiler intrinsics when you need it. Control resources with scope-based resource control (RAII) and your own memory allocators."
+        item={undefined}
+      />
+    </div>
   )
 }
