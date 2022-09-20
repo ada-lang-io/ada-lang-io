53705640b32ebddb6c871999d184e3a10936dfd4
onox <denkpadje@gmail.com>
2022-09-20 04:14:31 +0200

    Remove unneeded externalUrlRegex from Algolia config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -227,19 +227,12 @@ const config = {
         // Optional: see doc section below
         contextualSearch: true,
 
-        // Optional: Specify domains where the navigation should occur through
-        // window.location instead on history.push. Useful when our Algolia
-        // config crawls multiple documentation sites and we want to navigate
-        // with window.location.href to them.
-        externalUrlRegex: 'external\\.com|domain\\.com',
-
         // Optional: Algolia search parameters
+        // See https://www.algolia.com/doc/api-reference/search-api-parameters/
         searchParameters: {},
 
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
-
-        //... other Algolia params
       },
     }
 }
