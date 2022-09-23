814370d701de92f17b3ff8152e69345fffcf3573
onox <denkpadje@gmail.com>
2022-09-23 08:08:48 +0200

    Fix importing .scss, .json, and .ads/.adb files

---
+++ src/typings.d.ts
@@ -0,0 +1,11 @@
+declare module "*.module.scss"
+
+declare module "*.ads" {
+  const content: string
+  export default content
+}
+
+declare module "*.adb" {
+  const content: string
+  export default content
+}
