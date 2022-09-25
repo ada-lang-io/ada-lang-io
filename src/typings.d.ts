aad1f56f0d5b3199da857ddefd5d4733d9c65ee3
onox <denkpadje@gmail.com>
2022-09-25 23:47:57 +0200

    Improve type declaration of *.module.scss

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,7 @@
-declare module "*.module.scss"
+declare module "*.module.scss" {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
 
 declare module "*.ads" {
   const content: string
