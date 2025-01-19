da2164bb2ec7b75dd8c5933800c5894b27f8f5ee
Paul Jarrett <jarrett.paul.young@gmail.com>
2025-01-18 22:23:04 -0500

    Change main text on landing page.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const alireGitHubLatestRelease = "https://api.github.com/repos/alire-project/ali
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Ada Programming Language",
+  title: "ada-lang.io",
   tagline: "Readable, correct, performant",
   url: "https://ada-lang.io",
   baseUrl: "/",
